9f112f768dd5145f850d4b01ed1df7827a1a4081
Gary Guo <nbdd0121@hotmail.com>
2015-02-09 16:54:04 +0800

    Add test funtion to 24 points
    Add optimize (deoptimize, maybe)

--- 24points/24points.js
+++ 24points/24points.js
@@ -1,90 +1,109 @@
+'use strict';
+
 var episilon = 1e-8;
 
 function Constant(value) {
 	var ret = Object(value);
 	ret.type = 'Constant';
 	ret.value = value;
+	ret.cachedNormalization = ret;
+	ret.cachedResult = String(value);
 	return ret;
 }
 
-function Node(value, op, l, r) {
-	return MakeNode(value, op, [l, r]);
-}
-
-function UnaryNode(value, op, operand) {
-	return MakeNode(value, op, [operand]);
-}
-
-function MakeNode(value, op, arr) {
+function Node(value, op, arr) {
 	arr.type = op;
 	arr.value = value;
+	arr.cachedNormalization = null;
+	arr.cachedResult = null;
 	return arr;
 }
 
 /* Flatten a + node or * node */
 function flatten(node) {
-	var arr = [];
-	for (var i = 0; i < node.length; i++) {
-		var n = normalize(node[i]);
-		if (n.type !== node.type) {
-			arr.push(n);
-		} else {
-			Array.prototype.push.apply(arr, n);
-		}
+	var arr = normalize(node[0]);
+	if (arr.type === node.type) {
+		arr = arr.slice();
+	} else {
+		arr = [arr];
 	}
-	return MakeNode(node.value, node.type, arr);
+	var operand = normalize(node[1]);
+	if (operand.type !== node.type) {
+		arr.push(operand);
+	} else {
+		Array.prototype.push.apply(arr, operand);
+	}
+	return Node(node.value, node.type, arr);
 }
 
 function extractNeg(node) {
-	var operand = node[0];
-	if (operand.type !== '+') {
-		if (operand.value === 0) return operand;
-		return node;
+	var arr = normalize(node[0]);
+	if (arr.type === '+') {
+		arr = arr.slice();
+	} else {
+		arr = [arr];
 	}
-	var arr = [];
-	for (var i = 0; i < operand.length; i++) {
-		if (operand[i].type === 'neg') {
-			arr.push(operand[i][0]);
+	var operand = normalize(node[1]);
+	if (operand.type !== '+') {
+		if (operand.value === 0) {
+			arr.push(operand);
 		} else {
-			if (operand[i].value === 0) {
-				arr.push(operand[i]);
+			arr.push(Node(-operand.value, 'neg', [operand]));
+		}
+	} else {
+		for (var i = 0; i < operand.length; i++) {
+			if (operand[i].type === 'neg') {
+				arr.push(operand[i][0]);
 			} else {
-				arr.push(UnaryNode(-operand[i].value, 'neg', operand[i]));
+				if (operand[i].value === 0) {
+					arr.push(operand[i]);
+				} else {
+					arr.push(Node(-operand[i].value, 'neg', [operand[i]]));
+				}
 			}
 		}
 	}
-	return MakeNode(node.value, '+', arr);
+	return Node(node.value, '+', arr);
 }
 
 function extractRec(node) {
-	var operand = node[0];
-	if (operand.type !== '*') {
-		if (operand.value === 1 || operand.value === -1) return operand;
-		return node;
+	var arr = normalize(node[0]);
+	if (arr.type === '*') {
+		arr = arr.slice();
+	} else {
+		arr = [arr];
 	}
-	var arr = [];
-	for (var i = 0; i < operand.length; i++) {
-		if (operand[i].type === 'rec') {
-			arr.push(operand[i][0]);
+	var operand = normalize(node[1]);
+	if (operand.type !== '*') {
+		if (operand.value === 1 || operand.value === -1) {
+			arr.push(operand);
 		} else {
-			if (operand[i].value === 1 || operand[i].value === -1) {
-				arr.push(operand[i]);
+			arr.push(Node(1 / operand.value, 'rec', [operand]));
+		}
+	} else {
+		for (var i = 0; i < operand.length; i++) {
+			if (operand[i].type === 'rec') {
+				arr.push(operand[i][0]);
 			} else {
-				arr.push(UnaryNode(1 / operand[i].value, 'rec', operand[i]));
+				if (operand[i].value === 1 || operand[i].value === -1) {
+					arr.push(operand[i]);
+				} else {
+					arr.push(Node(1 / operand[i].value, 'rec', [operand[i]]));
+				}
 			}
 		}
 	}
-	return MakeNode(node.value, '*', arr);
+	return Node(node.value, '*', arr);
 }
 
 function reorder(node) {
 	node.sort(function(a, b) {
-		var as = codegen(a),
-			bs = codegen(b);
-		var aNeg = as[0] === '/' || as[0] === '-';
-		var bNeg = bs[0] === '/' || bs[0] === '-';
+		var aNeg = a.type === 'neg' || a.type === 'rec';
+		var bNeg = b.type === 'neg' || b.type === 'rec';
 		if (aNeg && !bNeg) return 1;
 		if (bNeg && !aNeg) return -1;
+		var as = codegen(a),
+			bs = codegen(b);
 		if (as.length !== bs.length)
 			return as.length - bs.length;
 		return as < bs ? -1 : 1;
@@ -93,32 +112,35 @@ function reorder(node) {
 }
 
 function normalize(node) {
+	if (node.cachedNormalization) {
+		return node.cachedNormalization;
+	}
+	var result;
 	switch (node.type) {
-		case 'Constant':
-			return node;
 		case '-':
-			return normalize(Node(node.value, '+', node[0], UnaryNode(-node[1].value, 'neg', normalize(node[1]))));
+			result = reorder(extractNeg(node));
+			break;
 		case '/':
-			return normalize(Node(node.value, '*', node[0], UnaryNode(1 / node[1].value, 'rec', normalize(node[1]))));
+			result = reorder(extractRec(node));
+			break;
 		case '+':
 		case '*':
-			return reorder(flatten(node));
-		case 'neg':
-			return extractNeg(node);
-		case 'rec':
-			return extractRec(node);
+			result = reorder(flatten(node));
+			break;
 		default:
 			throw 'Assertion Failure';
 	}
+	node.cachedNormalization = result;
+	return result;
 }
 
-function reduction(result, a, b, arr, i, target) {
-	arr[i] = Node(a.value + b.value, '+', a, b), enumeration(result, arr, target);
-	arr[i] = Node(a.value - b.value, '-', a, b), enumeration(result, arr, target);
-	arr[i] = Node(b.value - a.value, '-', b, a), enumeration(result, arr, target);
-	arr[i] = Node(a.value * b.value, '*', a, b), enumeration(result, arr, target);
-	arr[i] = Node(a.value / b.value, '/', a, b), enumeration(result, arr, target);
-	arr[i] = Node(b.value / a.value, '/', b, a), enumeration(result, arr, target);
+function reduction(result, arr, target, index, a, b) {
+	arr[index] = Node(a.value + b.value, '+', [a, b]), enumeration(result, arr, target);
+	arr[index] = Node(a.value - b.value, '-', [a, b]), enumeration(result, arr, target);
+	arr[index] = Node(b.value - a.value, '-', [b, a]), enumeration(result, arr, target);
+	arr[index] = Node(a.value * b.value, '*', [a, b]), enumeration(result, arr, target);
+	arr[index] = Node(a.value / b.value, '/', [a, b]), enumeration(result, arr, target);
+	arr[index] = Node(b.value / a.value, '/', [b, a]), enumeration(result, arr, target);
 }
 
 function enumeration(result, arr, target) {
@@ -134,7 +156,7 @@ function enumeration(result, arr, target) {
 		var a = arr[i];
 		for (var j = i; j < length - 1; j++) {
 			var b = arr[j + 1];
-			reduction(result, a, b, clone, j, target);
+			reduction(result, clone, target, j, a, b);
 			clone[j] = b;
 		}
 		clone[i] = a;
@@ -142,12 +164,11 @@ function enumeration(result, arr, target) {
 }
 
 function codegen(node) {
-	if (node.cachedResult)
+	if (node.cachedResult) {
 		return node.cachedResult;
+	}
 	var result;
 	switch (node.type) {
-		case 'Constant':
-			return String(node.value);
 		case 'neg':
 			result = '-' +
 				(node[0].type === '+' ? '(' + codegen(node[0]) + ')' : codegen(node[0]));
@@ -195,4 +216,33 @@ function calc(arr, target) {
 	arr = arr.map(Constant);
 	enumeration(result, arr, target);
 	return unique(result.map(normalize).map(codegen));
+}
+
+function test(length, maximum, target) {
+	var array = new Array(length);
+	var stats = {
+		total: 0,
+		success: 0,
+		fail: 0,
+		fails: []
+	};
+
+	function generateNumbers(index, start) {
+		if (index === length) {
+			stats.total++;
+			if (calc(array, target).length === 0) {
+				stats.fails.push(array.toString());
+				stats.fail++;
+			} else {
+				stats.success++;
+			}
+			return;
+		}
+		for (var i = start; i <= maximum; i++) {
+			array[index] = i;
+			generateNumbers(index + 1, i);
+		}
+	}
+	generateNumbers(0, 1);
+	return stats;
 }
